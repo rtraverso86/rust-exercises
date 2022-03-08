e9b7cd7f5c262492a9c094d67409ee3d3a0d3c69
Riccardo Traverso <rtraverso86@gmail.com>
2022-03-09 00:20:11 +0100

    fahrenheit: code improvement

--- fahrenheit/src/main.rs
+++ fahrenheit/src/main.rs
@@ -1,42 +1,54 @@
 use std::io;
 
-fn fahrenheit_to_celsius(f : f32) -> f32 {
-    ((f - 32.0) / 9.0) * 5.0
+fn fahrenheit_to_celsius(f: f32) -> f32 {
+    (f - 32.0) / 9.0 * 5.0
 }
 
-fn celsius_to_fahrenheit(c : f32) -> f32 {
+fn celsius_to_fahrenheit(c: f32) -> f32 {
     (c * 9.0 / 5.0) + 32.0
 }
 
-fn parse_temp(t : &str) -> (char, f32) {
+fn parse_temp(t: &str) -> Option<(char, f32)> {
     let t = t.trim().to_uppercase();
-    for suffix in ["°F", "°C"] {
-       match t.strip_suffix(suffix) {
-            Some(x) => return (suffix.chars().last().unwrap(), x.trim_end().parse().expect("NaN")),
-            None    => continue
-        }
-    }
-    ('\0', 0.0)
+
+    ["°F", "°C"]
+        .iter()
+        .filter_map(|suffix| {
+            t.strip_suffix(suffix).map(|x| {
+                (
+                    suffix.chars().last().unwrap(),
+                    x.trim_end().parse().expect("NaN"),
+                )
+            })
+        })
+        .next()
 }
 
-fn convert_temp(tuple : (char, f32)) -> (char, f32) {
+fn convert_temp(tuple: (char, f32)) -> (char, f32) {
     match tuple {
-        ('F', v)  => ('C', fahrenheit_to_celsius(v)),
-        ('C', v)  => ('F', celsius_to_fahrenheit(v)),
-        x => x
+        ('F', v) => ('C', fahrenheit_to_celsius(v)),
+        ('C', v) => ('F', celsius_to_fahrenheit(v)),
+        x => x,
     }
 }
 
 fn main() {
     loop {
         println!("Please enter a °C or °F value for conversion:");
+
         let mut input = String::new();
         io::stdin()
             .read_line(&mut input)
             .expect("Failed to read line");
-        if input == "\n" { break }
-        let (u, v) = convert_temp(parse_temp(&input));
-        if u == '\0' { continue }
-        println!("The converted temperature is {}°{}", v, u);
+
+        if input == "\n" {
+            break;
+        }
+        if let Some((u, v)) = parse_temp(&input).map(convert_temp) {
+            println!("The converted temperature is {}°{}", v, u);
+        }
+
+        break;
     }
 }
+
