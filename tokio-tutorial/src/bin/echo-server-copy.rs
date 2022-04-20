be093a232374318dd091c2eb0190040f05b9f1ba
Riccardo Traverso <rtraverso86@gmail.com>
2022-04-20 19:14:54 +0200

    tokio-tutorial: echo-server-copy

---
+++ tokio-tutorial/src/bin/echo-server-copy.rs
@@ -0,0 +1,18 @@
+use tokio::io;
+use tokio::net::TcpListener;
+
+#[tokio::main]
+async fn main() -> io::Result<()> {
+    let listener = TcpListener::bind("127.0.0.1:6142").await?;
+    
+    loop {
+        let (mut socket, _)  = listener.accept().await?;
+        let peer = socket.peer_addr().unwrap();
+        tokio::spawn(async move {
+            let (mut rx, mut tx) = io::split(socket);
+            if io::copy(&mut rx, &mut tx).await.is_err() {
+                eprintln!("[{}] failed to copy", &peer);
+            }
+        });
+    }
+}
